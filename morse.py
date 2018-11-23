b785b7f4ed2fc5f7dc22b6276544782616aa7986
Phillip Deremer <phillip@deremer.de>
2018-11-23 18:11:02 +0100

    finished morse part

--- morse.py
+++ morse.py
@@ -1,3 +1,4 @@
+import argparse
 from gpio_guard import *
 import RPi.GPIO as gpio
 import time as T
@@ -65,6 +66,12 @@ ALPHABET = {
   "@": [1, 3, 3, 1, 3, 1]
 }
 
+ON = "■"
+OFF = "·"
+
+DEF_SPEED = 0.15
+DEF_GPIO = 3
+
 def str2morse(str):
   str = str.upper()
   result = []
@@ -81,15 +88,17 @@ def str2morse(str):
   return result
 
 def displayMorse(morse):
+  global ON, OFF
   result = ""
   for b in morse:
     if b:
-      result += "="
+      result += ON
     else:
-      result += "_"
+      result += OFF
   return result
 
-def outMorse(morse, IO, doPrint = False, speed = 0.125):
+def outMorse(morse, IO = DEF_GPIO, doPrint = True, speed = DEF_SPEED):
+  #global ON, OFF
   with GPIO_G():
     gpio.setmode(gpio.BCM)
     gpio.setup(IO, gpio.OUT)
@@ -98,21 +107,34 @@ def outMorse(morse, IO, doPrint = False, speed = 0.125):
       gpio.output(IO, b)
       if doPrint:
         if b:
-          sys.stdout.write("=")
+          sys.stdout.write(ON)
         else:
-          sys.stdout.write("_")
+          sys.stdout.write(OFF)
         sys.stdout.flush()
       T.sleep(speed)
     print("")
 
 def main(argv):
-  for arg in argv:
-    m = str2morse(arg)
-    #print(displayMorse(m))
-    outMorse(m, 3, True)
+  print(str(argv)[10:-1])
+  run = True
+  while run:
+    for arg in argv.morse:
+      m = str2morse(arg)
+      #print(displayMorse(m))
+      outMorse(m, int(argv.gpio), True, float(argv.speed))
+    run = argv.repeat
 
 if __name__=="__main__":
-  if len(sys.argv) > 1:
-    main(sys.argv[1:])
-  else:
-    main([])
+  parser = argparse.ArgumentParser()
+  parser.add_argument('morse', nargs='+', help="Here goes the data that shall be displayed as morse code.")
+  parser.add_argument('-s','--speed', default=DEF_SPEED, required=False, help="Sets the speed which is used to display the morse code. Default: " + str(DEF_SPEED) + " sec")
+  parser.add_argument('-io','--gpio', default=DEF_GPIO, required=False, help="Sets the GPIO which is used to output the morse code. Default: "+ str(DEF_GPIO))
+  parser.add_argument('-r','--repeat', required=False, action='store_true', help="...")
+
+  args = parser.parse_args()
+  
+  try:  
+    main(args)
+  except KeyboardInterrupt:
+    print("\nTransmission canceled!")
+
